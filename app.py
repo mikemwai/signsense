ab6026c752a2f030e2ad1c57d41d4e1fe3de3269
mikemwai <mwai.michael@strathmore.edu>
2024-07-09 15:03:36 +0300

    frontend updates

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/', methods=['GET'])
+def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
