be298732a302194238912fe2565070ba4afde60b
mikemwai <mwai.michael@strathmore.edu>
2024-07-12 18:42:02 +0300

    frontend updates: user module

--- app.py
+++ app.py
@@ -19,5 +19,13 @@ def authentication():
     return render_template('authentication.html')
 
 
+@app.route('/model', methods=['GET'])
+def model():
+    return render_template('model.html')
+
+@app.route('/user_dashboard', methods=['GET'])
+def user_dashboard():
+    return render_template('user_dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
