d9f6c9c416fd2a6baf88375e9088044f7eda452b
mikemwai <mwai.michael@strathmore.edu>
2024-08-07 13:09:00 +0300

    UI changes

--- app.py
+++ app.py
@@ -6,27 +6,27 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    return render_template('pages/index.html')
 
 
 @app.route('/about', methods=['GET'])
 def about():
-    return render_template('about.html')
+    return render_template('pages/about.html')
 
 
 @app.route('/authentication', methods=['GET'])
 def authentication():
-    return render_template('authentication.html')
+    return render_template('pages/authentication.html')
 
 
 @app.route('/model', methods=['GET'])
 def model():
-    return render_template('model.html')
+    return render_template('pages/model.html')
 
 
 @app.route('/user_dashboard', methods=['GET'])
 def user_dashboard():
-    return render_template('user_dashboard.html')
+    return render_template('pages/user_dashboard.html')
 
 
 if __name__ == '__main__':
